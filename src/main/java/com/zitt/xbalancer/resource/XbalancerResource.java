78c347b0f828d1b7c6d683d93b52798a500f892c
Lukasz Krzywda <lukasz.krzywda@gmail.com>
2021-03-06 23:15:29 -0500

    Fixing the wrapper task, updating year of the licence

--- src/main/java/com/zitt/xbalancer/resource/XbalancerResource.java
+++ src/main/java/com/zitt/xbalancer/resource/XbalancerResource.java
@@ -1,7 +1,7 @@
 /**
  * MIT License
  * <p>
- * Copyright (c) 2018-2020 Lukasz Krzywda
+ * Copyright (c) 2018-2021 Lukasz Krzywda
  * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
